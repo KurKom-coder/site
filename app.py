67771e50f219cc8a2a92af6c47061856b095cee0
KurKom <72676424+KurKom-coder@users.noreply.github.com>
2025-02-28 06:29:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -185,4 +185,5 @@ threading.Thread(target=cleanup_files, daemon=True).start()
 
 if __name__ == '__main__':
     os.makedirs("uploads", exist_ok=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
